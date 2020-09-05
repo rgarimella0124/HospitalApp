8d9fdcb24874b40b08bcfb3769f386a94d543f52
Ram Simran G <ram@neokred.tech>
2020-09-05 19:00:10 +0530

    First Commit

--- App.js
+++ App.js
@@ -1,16 +1,55 @@
-import * as React from "react";
-import { View, Text } from "react-native";
+import React from "react";
+import { NavigationContainer, StackActions } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Image } from "react-native";
+import Home from "./screens/home";
+import Mission from "./screens/mission";
+const Stack = createStackNavigator();
 
-export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{
+              headerTransparent: true,
+              headerLeft: () => (
+                <Image source={require("./assets/drawericon.png")} style={{}} />
+              ),
+              title: "",
+              headerRight: () => (
+                <Image
+                  source={require("./assets/user.png")}
+                  style={{ marginHorizontal: 20, marginTop: 40 }}
+                />
+              ),
+            }}
+          />
+          <Stack.Screen
+            name="Mission"
+            component={Mission}
+            options={{
+              headerTransparent: true,
+              headerLeft: () => (
+                <Image
+                  source={require("./assets/blackmenu.png")}
+                  style={{ marginHorizontal: 20, marginTop: 40 }}
+                />
+              ),
+              title: "",
+              headerRight: () => (
+                <Image
+                  source={require("./assets/user.png")}
+                  style={{ marginHorizontal: 35, marginTop: 45 }}
+                />
+              ),
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
